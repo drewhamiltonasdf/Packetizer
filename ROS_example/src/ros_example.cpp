1099f9d9e5a1f72707f57dee20749e195c614ae3
Drew Hamilton <contact@drewhamilton.net>
2020-12-24 15:04:53 -0500

    Put CRC check into example

--- ROS_example/src/ros_example.cpp
+++ ROS_example/src/ros_example.cpp
@@ -22,7 +22,10 @@
 #define ROS_TEENSY_CRC
 
 #define PACKETIZER_USE_CRC_AS_DEFAULT
+#define PACKETIZER_USE_INDEX_AS_DEFAULT
+
 bool USE_CRC = true;
+bool USE_INDEX = true;
 
 int main(int argc, char **argv)
 {
@@ -33,31 +36,39 @@ int main(int argc, char **argv)
     Packetizer::Packet packet_in;
     
     //Add in a non-char sized thing to our packet using this function defined in type_packer.h
-    double asdf1 = -60.0234; push_back_type(packet_in.data, asdf1);
-    double asdf2 = -63.1234; push_back_type(packet_in.data, asdf2);
-    double asdf3 =  67.934; push_back_type(packet_in.data, asdf3);
+    uint8_t asdf1 = 'A'; push_back_type(packet_in.data, asdf1);
+    uint8_t asdf2 = 'B'; push_back_type(packet_in.data, asdf2);
+    uint8_t asdf3 = 'C'; push_back_type(packet_in.data, asdf3);
     
     // This will cause an error, we can't have a variadic packet with multiple types
     // float asdf4 =   17.934; push_back_type(packet_in.data, asdf4);    
+    
+    packet_in.index = 123;
 
-    const auto& p_buff = Packetizer::encode(packet_in.data.data(), packet_in.data.size(), USE_CRC);
-    const auto& packet_out = Packetizer::decode(p_buff.data.data(), p_buff.data.size(), USE_CRC);
+    const auto& p_buff = Packetizer::encode(packet_in.index, packet_in.data.data(), packet_in.data.size(), USE_CRC);
+    
+    //inline const Packet& decode(const uint8_t* data, const size_t size, const bool b_index, const bool b_crc)
+    const auto& packet_out = Packetizer::decode(p_buff.data.data(), p_buff.data.size(), USE_INDEX, USE_CRC);
 
-        std::cout << "input   = "; for (const auto& p : packet_in.data) { std::cout << "[" << p << "]: " << (int)p << std::endl;  } 
-        std::cout << "encoded = "; for (const auto& p : p_buff.data) {std::cout << "[" << p << "]: " << (int)p << std::endl;  } 
-        std::cout << "decoded = "; for (const auto& p : packet_out.data) {std::cout << "[" << p << "]: " << (int)p << std::endl;  } 
+        std::cout << "input   = " << std::endl << std::endl; for (const auto& p : packet_in.data) { std::cout << "[" << p << "]: " << (int)p << std::endl;  }  std::cout << std::endl;
+        std::cout << "encoded = " << std::endl << std::endl; for (const auto& p : p_buff.data) {std::cout << "[" << p << "]: " << (int)p << std::endl;  }  std::cout << std::endl;
+        std::cout << "decoded = " << std::endl << std::endl; for (const auto& p : packet_out.data) {std::cout << "[" << p << "]: " << (int)p << std::endl;  } std::cout << std::endl;
 
-        std::cout << "index_out = " << packet_out.index;
+        std::cout << "index_out = [" << (int)packet_out.index << "]" << std::endl;
 
         if (packet_in.data != packet_out.data) {std::cout << "test 1 failed!"<< std::endl;}
         else                         
-            {std::cout << "test 1 success!"<< std::endl;}
+            {std::cout << std::endl << "test 1 success!"<< std::endl;}
+
+    std::cout << "CRC is expected to be = " << (int)crcx::crc8(packet_in.data.data(), packet_in.data.size()) << std::endl;
+    std::cout << "CRC from encoded packet is = " << (int)p_buff.data.at(static_cast<int>(p_buff.data.size()) - 2) << std::endl;
+    //std::cout << "CRC = " << (int)FastCRC::crc8(packet_out.data.data(), p_buff.data.size());
 
     // Here we unpack our packet into doubles. But you can change the template type
     // and get any kind of type you want as long as that's what you made the packet out
     // of in the first place. In other words, if you have a datatype that is 4 bytes long
     // your packet ought to be divisible by 4. If not, you'll get an error.
-    std::vector<double> new_container;
+    std::vector<uint8_t> new_container;
     bool success = unpack_type(packet_out.data, new_container);
 
     if (success)
